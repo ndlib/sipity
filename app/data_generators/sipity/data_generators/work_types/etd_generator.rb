dd4afc4accdeb1a875fb7745c9cd64d40ab9787d
Rajesh Balekai <rbalekai@nd.edu>
2015-09-15 11:46:06 -0400

    Wrap up etd batch ingester
    Create user and group for etd batch user
    Create appropriate action and permissions
    Create SubmitForIngestForm
    Create rails runner for etd batch ingester

--- app/data_generators/sipity/data_generators/work_types/etd_generator.rb
+++ app/data_generators/sipity/data_generators/work_types/etd_generator.rb
@@ -14,6 +14,7 @@ module Sipity
         ]
         GRADUATE_SCHOOL_REVIEWERS = 'Graduate School Reviewers'
         CATALOGERS = "Catalogers"
+        ETD_INGESTORS = "Etd Ingestors"
 
         def self.call(**keywords)
           new(**keywords).call
@@ -52,7 +53,8 @@ module Sipity
                 'creating_user',
                 'etd_reviewer',
                 'advisor',
-                'cataloger'
+                'cataloger',
+                'batch_ingestor'
               ].each do |role_name|
                 etd_strategy_roles[role_name] = find_or_initialize_or_create!(
                   context: etd_strategy,
@@ -63,6 +65,7 @@ module Sipity
 
               etd_reviewer = etd_strategy_roles.fetch('etd_reviewer')
               cataloger = etd_strategy_roles.fetch('cataloger')
+              batch_ingestor = etd_strategy_roles.fetch('batch_ingestor')
 
               find_or_initialize_or_create!(
                 context: etd_reviewer,
@@ -76,6 +79,12 @@ module Sipity
                 actor: Conversions::ConvertToProcessingActor.call(Models::Group.find_or_create_by!(name: CATALOGERS))
               )
 
+              find_or_initialize_or_create!(
+                context: batch_ingestor,
+                receiver: batch_ingestor.strategy_responsibilities,
+                actor: Conversions::ConvertToProcessingActor.call(Models::Group.find_or_create_by!(name: ETD_INGESTORS))
+              )
+
               etd_states = {}
               [
                 'new',
@@ -85,7 +94,9 @@ module Sipity
                 'grad_school_changes_requested',
                 'ready_for_ingest',
                 'ready_for_cataloging',
-                'back_from_cataloging'
+                'back_from_cataloging',
+                'ingesting',
+                'ingested'
               ].each do |state_name|
                 etd_states[state_name] = find_or_initialize_or_create!(
                   context: etd_strategy,
@@ -119,7 +130,8 @@ module Sipity
                 { action_name: 'send_to_cataloging', resulting_state_name: 'ready_for_cataloging',seq: 1, allow_repeat_within_current_state: false },
                 { action_name: 'send_back_to_grad_school', resulting_state_name: 'back_from_cataloging',seq: 2, allow_repeat_within_current_state: true },
                 { action_name: 'cataloging_complete', resulting_state_name: 'ready_for_ingest',seq: 1, allow_repeat_within_current_state: false },
-                { action_name: 'ingest_with_postponed_cataloging', resulting_state_name: 'ready_for_ingest',seq: 1, allow_repeat_within_current_state: false }
+                { action_name: 'ingest_with_postponed_cataloging', resulting_state_name: 'ready_for_ingest',seq: 1, allow_repeat_within_current_state: false },
+                { action_name: 'submit_for_ingest', resulting_state_name: 'ingesting', seq: 1, allow_repeat_within_current_state: false }
               ].each do |structure|
                 action_name = structure.fetch(:action_name)
                 resulting_state = structure[:resulting_state_name] ? etd_states.fetch(structure[:resulting_state_name]) : nil
@@ -243,6 +255,10 @@ module Sipity
                   ['under_grad_school_review', 'grad_school_changes_requested'],
                   ['grad_school_requests_change', 'send_to_cataloging', 'ingest_with_postponed_cataloging'],
                   ['etd_reviewer']
+                ],[
+                  ['ready_for_ingest'],
+                  ['submit_for_ingest'],
+                  ['batch_ingestor']
                 ]
               ].each do |originating_state_names, action_names, role_names|
                 Array.wrap(originating_state_names).each do |originating_state_name|
