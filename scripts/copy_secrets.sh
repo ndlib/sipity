adbaed0f6ca4b289f7555f6f6842acc6649ae71d
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2017-03-01 16:39:34 -0500

    Placeholder script for secret management
    The build server will be responsible for selecting the appropriate
    secret file to be pushed to the app server.

---
+++ scripts/copy_secrets.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Copy the relevant secrets from the build server to the app server.
+#
+# TODO update script to perform the correct function
+#
+# usage:
+#   ./update_secrets.sh <name of secret repo>
+
+secret_repo=$1
+
+if [ -d $secret_repo ]; then
+    echo "=-=-=-=-=-=-=-= delete $secret_repo"
+    rm -rf $secret_repo
+fi
+echo "=-=-=-=-=-=-=-= git clone $secret_repo"
+git clone "git@git.library.nd.edu:$secret_repo"
+
+files_to_copy="
+    config/application.yml
+    config/database.yml
+    config/newrelic.yml
+    config/environment_bootstrapper.rb
+    config/locales/site-specific.yml
+    app/assets/stylesheets/theme/_default.scss
+    "
+
+for f in $files_to_copy; do
+    echo "=-=-=-=-=-=-=-= copy $f"
+    if [ -f $secret_repo/sipity/$f ];
+    then
+        cp $secret_repo/sipity/$f $f
+    else
+        echo "Fatal Error: File $f does not exist in $secret_repo/sipity"
+        exit 1
+    fi
+done
