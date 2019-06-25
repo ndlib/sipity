62802d99200dedf05b5520ae29d1147951c603f9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2019-06-25 13:08:18 -0400

    Adding polymorphic_type power converter
    There exists a convert_to_polymorphic_type module. I want to replace
    that. First, I'd prefer power converts. Second, I see numerous
    deprecation warnings in the specs concerning passing a string instead
    of a class (which is partially from the polymorphic_type).

---
+++ spec/conversions/power_converters/polymorphic_type_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'PowerConverter' do
+  context 'polymorphic_type' do
+    [
+      [User.new, 'User'],
+      [User, 'User']
+    ].each_with_index do |(to_convert, expected), index|
+      it "will convert #{to_convert.inspect} to #{expected} (Scenario ##{index}" do
+        expect(PowerConverter.convert(to_convert, to: :polymorphic_type)).to eq(expected)
+      end
+    end
+
+    [
+      :User, 'User'
+    ].each do |to_convert|
+      it "will raise an exception for #{to_convert.inspect}" do
+        expect { PowerConverter.convert(to_convert, to: :polymorphic_type) }.to raise_error(PowerConverter::ConversionError)
+      end
+    end
+  end
+end
