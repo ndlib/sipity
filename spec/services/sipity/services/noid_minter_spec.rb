294a24afd3c720cb17d78072a7ca1e2b75c624db
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2020-04-15 10:27:14 -0400

    Adding full NOIDs integration
    Prior to this commit, Sipity did not have a means of testing against a
    running [our NOIDs server](https://github.com/ndlib/noids). This
    commit refactors the specs and minter implementation to make it
    possible to run an integration test against a "live" NOIDs server.
    
    The refactor preserves the lambda method signature in the [application
    config][1] for the configured default_pid_minter.
    
    Dependent on ndlib/noids_client#5
    
    [1]:./config/application.rb

--- spec/services/sipity/services/noid_minter_spec.rb
+++ spec/services/sipity/services/noid_minter_spec.rb
@@ -1,28 +1,47 @@
 require 'rails_helper'
 require 'sipity/services/noid_minter'
+require 'noids_client/integration_test' if ENV["WITH_NOIDS_SERVER"]
+
 module Sipity
   module Services
     describe NoidMinter do
-      include ::Sipity::Services::NoidMinter
       let(:pid) { "pid" }
-      let(:connection) { double(mint: [pid]) }
-
-      context '#call' do
+      let(:pool) { double("Minter", mint: [pid]) }
+      let(:connection) { double("Connection", get_pool: pool) }
+      context '.call' do
         it 'will mint a pid' do
-          expect(described_class).to receive(:connection).
-            and_return(connection)
+          expect(NoidsClient::Connection).to(
+            receive(:new).with("#{Figaro.env.noid_server}:#{Figaro.env.noid_port}").and_return(connection)
+          )
+          expect(connection).to(
+            receive(:get_pool).with(Figaro.env.noid_pool).and_return(pool)
+          )
           expect(described_class.call).to eq(pid)
         end
       end
 
-      context '#connections' do
-        let(:first_connection) { described_class.connection }
-        it 'verify connections are cached' do
-          expect(::NoidsClient::Connection).to receive_message_chain(:new, :get_pool).
-            and_return(connection)
-          next_connection = described_class.connection
-          expect(first_connection.object_id).to eq(next_connection.object_id)
+      if ENV["WITH_NOIDS_SERVER"]
+        context 'with "live" NOIDs server' do
+          # A bit of hard-coded magic. These are the default hosts and ports
+          let(:noid_server) { "http://localhost" }
+          let(:noid_port) { "13001" }
+          let(:noid_pool_name) { "test" }
+          it 'will mint a pid' do
+            ::NoidsClient::IntegrationTest::NoidServerRunner.new.run do
+              # Sipity assumes that the noid_pool already exists. It is the one
+              # CurateND has been using.
+              noid_minter = described_class.new(server: noid_server, port: noid_port, pool_name: noid_pool_name)
+              noid_minter.connection.new_pool("test", ".sddd")
+              expect(noid_minter.call).to eq("000")
+              expect(noid_minter.call).to eq("001")
+            end
+          end
         end
+      else
+        $stdout.puts %(\n\n\n)
+        $stdout.puts %(Skipping "live" test of noids minter.)
+        $stdout.puts %(To run with a live menter, run the test suite with ENV["WITH_NOIDS_SERVER"] of true)
+        $stdout.puts %(\n\n\n)
       end
     end
   end
