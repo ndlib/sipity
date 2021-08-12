c4ae6b88d06778047e9c0479ef42a4076c22d296
Mark Suhovecky <suhovecky@nd.edu>
2021-08-12 10:41:04 -0400

    feat: use rails config for profiler_users (curate-291)

--- app/controllers/sipity/controllers/authenticated_controller.rb
+++ app/controllers/sipity/controllers/authenticated_controller.rb
@@ -2,6 +2,7 @@ module Sipity
   module Controllers
     # For those controllers that require authentication
     class AuthenticatedController < ::ApplicationController
+     before_action  :enable_profiling
      # Enable profiling for data_admin users
 
       def authenticate_user!
@@ -21,18 +22,14 @@ module Sipity
       # @todo With Cogitate this will need to be revisited
       def current_user
         super
-        current_user_enables_profiler! if profiling_enabled?
-        return @current_user if @current_user
+        if @current_user
+          return @current_user
+        end
+
         authenticate_user!
         @current_user
       end
 
-      def current_user_enables_mini_profiler!
-        return unless @current_user
-        return unless Sipity::DataGenerators::WorkTypes::EtdGenerator::DATA_ADMINISTRATORS.include?(current_user.username)
-        Rack::MiniProfiler.authorize_request
-        return
-      end
       private
 
       # @todo With Cogitate this will need to be revisited
@@ -41,9 +38,19 @@ module Sipity
         return false unless group_name
         Sipity::Models::Group.find_by(name: group_name, api_key: group_api_key) || false
       end
+       
+      def enable_profiling
+        return false unless profiling_enabled?
+        return false unless current_user && is_profiler_user?(current_user.username)
+        Rack::MiniProfiler.authorize_request
+      end
  
       def profiling_enabled?
         Rails.configuration.use_profiler == true
+      end
+ 
+      def is_profiler_user?(user:)
+        Rails.configuration.profiler_users.include?(user)
       end
     end
   end
