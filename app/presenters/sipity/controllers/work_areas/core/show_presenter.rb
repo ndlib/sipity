2dbec3660a76390a0bf830f382f2dd342102a47e
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2021-08-12 09:49:41 -0400

    Adding additional attributes for /area/etds
    While technically not necessary, I wanted to be specific in declaring
    what attributes we're including.
    
    I also took the time to refactor the public/private declarations.  It's
    a bit more idiomatic.

--- app/presenters/sipity/controllers/work_areas/core/show_presenter.rb
+++ app/presenters/sipity/controllers/work_areas/core/show_presenter.rb
@@ -7,23 +7,32 @@ module Sipity
       module Core
         # Responsible for presenting a work area
         class ShowPresenter < Controllers::Visitors::Core::WorkAreaPresenter
-          public def filter_form(dom_class: 'form-inline', method: 'get', &block)
+          def filter_form(dom_class: 'form-inline', method: 'get', &block)
             form_tag(request.path, method: method, class: dom_class, &block)
           end
 
-          public def works
+          def works
             @works ||= repository.find_works_via_search(criteria: search_criteria)
           end
 
-          public def paginate_works
+          def paginate_works
             paginate(works)
           end
 
-          private def search_criteria
+          private
+
+          def search_criteria
             @search_criteria ||= begin
               Parameters::SearchCriteriaForWorksParameter.new(
-                user: current_user, processing_state: work_area.processing_state, page: work_area.page, order: work_area.order,
-                repository: repository, work_area: work_area, q: work_area.q, submission_window: work_area.submission_window
+                user: current_user,
+                processing_state: work_area.processing_state,
+                page: work_area.page,
+                order: work_area.order,
+                repository: repository,
+                work_area: work_area,
+                q: work_area.q,
+                submission_window: work_area.submission_window,
+                additional_attributes: ["author_name", "submission_date"]
               )
             end
           end
